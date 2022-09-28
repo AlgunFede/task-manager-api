d00ca75840ff42f206cb4fc9581bb0094ebaa0c8
AlgunFede <federexus@hotmail.com>
2022-09-28 12:38:38 -0300

    Little fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-require('./src/db/mongoose');
-const userRouter = require('./src/routers/user');
-const taskRouter = require('./src/routers/task')
+require('./db/mongoose');
+const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task')
 
 const app = express();
 const port = process.env.PORT;
